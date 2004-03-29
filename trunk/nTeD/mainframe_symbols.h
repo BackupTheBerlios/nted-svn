1b5153fa59e0acd5cdd590838f215e8c70cd1ab3
kintups <kintups@62037c61-b3d6-0310-b8c7-ccd26a683bc2>
2004-03-29 17:37:09 +0000

    Main Frame Strings

---
+++ trunk/nTeD/mainframe_symbols.h
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        mainframe_symbols.h
+// Purpose:     String symbols
+// Author:      Kintups
+// Modified by: 
+// Created:     02/25/04 16:07:25
+// RCS-ID:      
+// Copyright:   
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#define CONECTAR _("Conectar")
+#define CANALES _("Canales")
+#define BARAJAS _("Barajas")
+#define INFO _("Información")
+#define PREFERENCIAS _("Preferencias")
