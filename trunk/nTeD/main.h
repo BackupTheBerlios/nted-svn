da0df9780d4a27a16e1492aa1931f6688e825252
kintups <kintups@62037c61-b3d6-0310-b8c7-ccd26a683bc2>
2004-04-16 09:27:12 +0000

    Main Header

---
+++ trunk/nTeD/main.h
@@ -0,0 +1,31 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <wx/wx.h>
+#include <wx/image.h>
+#include "wx/xrc/xmlres.h"          // XRC XML resouces
+#include "mainframe.h"
+
+class MainApp : public wxApp
+{
+  public:
+    bool OnInit(void);
+};
+
+DECLARE_APP(MainApp)
+
+/*****************************************************************
+ * Create a new application object: this macro will allow
+ * wxWindows to create the application object during program
+ * execution (it's better than using a static object for many
+ * reasons) and also declares the accessor function wxGetApp()
+ * which will return the reference of the right type (i.e. MyApp
+ * and not wxApp)
+ *
+ *****************************************************************/
+
+IMPLEMENT_APP(MainApp)
+
+
+#endif _MAIN_H__
+
