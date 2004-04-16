38ac9d99e42bf3a3c5870e35e186246f54877c4a
kintups <kintups@62037c61-b3d6-0310-b8c7-ccd26a683bc2>
2004-04-16 09:18:21 +0000

    Chat Dialog Header

--- trunk/nTeD/chatdialog.h
+++ trunk/nTeD/chatdialog.h
@@ -3,7 +3,7 @@
 // Purpose:     
 // Author:      Kintups
 // Modified by: 
-// Created:     02/27/04 09:53:08
+// Created:     04/14/04 19:05:22
 // RCS-ID:      
 // Copyright:   
 // Licence:     
@@ -12,7 +12,7 @@
 #ifndef _CHATDIALOG_H_
 #define _CHATDIALOG_H_
 
-#ifdef __GNUG__
+#if defined(__GNUG__) && !defined(__APPLE__)
 #pragma interface "chatdialog.cpp"
 #endif
 
@@ -22,6 +22,7 @@
 
 ////@begin includes
 #include "chatdialog_symbols.h"
+#include "wx/listctrl.h"
 ////@end includes
 
 /*!
@@ -37,6 +38,11 @@
 
 ////@begin control identifiers
 #define ChatDialog 10000
+#define SYMBOL_WXCHATDIALOG_STYLE wxCLOSE_BOX
+#define SYMBOL_WXCHATDIALOG_TITLE _("Chat")
+#define SYMBOL_WXCHATDIALOG_IDNAME ChatDialog
+#define SYMBOL_WXCHATDIALOG_SIZE wxSize(580, 350)
+#define SYMBOL_WXCHATDIALOG_POSITION wxDefaultPosition
 #define ID_TEXTCTRL 10001
 #define ID_TEXTCTRL1 10002
 #define EnviarButton 10003
@@ -53,38 +59,38 @@
 #endif
 
 /*!
- * ChatDialog class declaration
+ * wxChatDialog class declaration
  */
 
-class ChatDialog: public wxDialog
+class wxChatDialog: public wxPanel
 {    
-    DECLARE_CLASS( ChatDialog )
+    DECLARE_CLASS( wxChatDialog )
     DECLARE_EVENT_TABLE()
 
 public:
     /// Constructors
-    ChatDialog( );
-    ChatDialog( wxWindow* parent, wxWindowID id = -1, const wxString& caption = _("Chat"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU );
+    wxChatDialog( );
+    wxChatDialog( wxWindow* parent, wxWindowID id = SYMBOL_WXCHATDIALOG_IDNAME, const wxPoint& pos = SYMBOL_WXCHATDIALOG_POSITION, const wxSize& size = SYMBOL_WXCHATDIALOG_SIZE, long style = SYMBOL_WXCHATDIALOG_STYLE );
 
     /// Creation
-    bool Create( wxWindow* parent, wxWindowID id = -1, const wxString& caption = _("Chat"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU );
+    bool Create( wxWindow* parent, wxWindowID id = SYMBOL_WXCHATDIALOG_IDNAME, const wxPoint& pos = SYMBOL_WXCHATDIALOG_POSITION, const wxSize& size = SYMBOL_WXCHATDIALOG_SIZE, long style = SYMBOL_WXCHATDIALOG_STYLE );
 
     /// Creates the controls and sizers
     void CreateControls();
 
-////@begin ChatDialog event handler declarations
+////@begin wxChatDialog event handler declarations
 
-////@end ChatDialog event handler declarations
+////@end wxChatDialog event handler declarations
 
-////@begin ChatDialog member function declarations
+////@begin wxChatDialog member function declarations
 
-////@end ChatDialog member function declarations
+////@end wxChatDialog member function declarations
 
     /// Should we show tooltips?
     static bool ShowToolTips();
 
-////@begin ChatDialog member variables
-////@end ChatDialog member variables
+////@begin wxChatDialog member variables
+////@end wxChatDialog member variables
 };
 
 #endif
