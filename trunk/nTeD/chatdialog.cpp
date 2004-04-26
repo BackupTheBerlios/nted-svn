26ad152c5d0d8cd575c70a53efbfb556e9fdd619
kintups <kintups@62037c61-b3d6-0310-b8c7-ccd26a683bc2>
2004-04-26 09:38:16 +0000

    Chat Dialog Source

--- trunk/nTeD/chatdialog.cpp
+++ trunk/nTeD/chatdialog.cpp
@@ -45,6 +45,8 @@ IMPLEMENT_CLASS( wxChatDialog, wxPanel )
 BEGIN_EVENT_TABLE( wxChatDialog, wxPanel )
 
 ////@begin wxChatDialog event table entries
+    EVT_BUTTON( EnviarButtonID, wxChatDialog::OnEnviarbuttonClick )
+
 ////@end wxChatDialog event table entries
 
 END_EVENT_TABLE()
@@ -69,6 +71,11 @@ wxChatDialog::wxChatDialog( wxWindow* parent, wxWindowID id, const wxPoint& pos,
 bool wxChatDialog::Create( wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style )
 {
 ////@begin wxChatDialog member initialisation
+    MensajesTextCtrl = NULL;
+    MensajeTextCtrl = NULL;
+    EnviarButton = NULL;
+    UsuariosListCtrl = NULL;
+    CanalesListCtrl = NULL;
 ////@end wxChatDialog member initialisation
 
 ////@begin wxChatDialog creation
@@ -80,6 +87,10 @@ bool wxChatDialog::Create( wxWindow* parent, wxWindowID id, const wxPoint& pos, 
     GetSizer()->SetSizeHints(this);
     Centre();
 ////@end wxChatDialog creation
+    UsuariosListCtrl->SetSizeHints(200,100);
+    CanalesListCtrl->SetSizeHints(200,100);
+    Layout();
+
     return TRUE;
 }
 
@@ -110,7 +121,8 @@ void wxChatDialog::CreateControls()
     item3->Add(item4, 1, wxGROW|wxGROW|wxALL|wxADJUST_MINSIZE, 0);
 
     wxTextCtrl* item5 = new wxTextCtrl;
-    item5->Create( item1, ID_TEXTCTRL, _T(""), wxDefaultPosition, wxSize(400, 300), wxTE_MULTILINE );
+    item5->Create( item1, MensajesTextCtrlID, _T(""), wxDefaultPosition, wxSize(400, 300), wxTE_MULTILINE );
+    MensajesTextCtrl = item5;
     item4->Add(item5, 1, wxGROW|wxGROW|wxALL, 0);
 
     wxFlexGridSizer* item6 = new wxFlexGridSizer(1, 2, 0, 5);
@@ -119,11 +131,13 @@ void wxChatDialog::CreateControls()
     item4->Add(item6, 1, wxGROW|wxALIGN_BOTTOM|wxALL, 0);
 
     wxTextCtrl* item7 = new wxTextCtrl;
-    item7->Create( item1, ID_TEXTCTRL1, _T(""), wxDefaultPosition, wxDefaultSize, 0 );
+    item7->Create( item1, MensajeTextCtrlID, _T(""), wxDefaultPosition, wxDefaultSize, 0 );
+    MensajeTextCtrl = item7;
     item6->Add(item7, 1, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 0);
 
     wxButton* item8 = new wxButton;
-    item8->Create( item1, EnviarButton, _("Enviar"), wxDefaultPosition, wxDefaultSize, 0 );
+    item8->Create( item1, EnviarButtonID, _("Enviar"), wxDefaultPosition, wxDefaultSize, 0 );
+    EnviarButton = item8;
     item8->SetDefault();
     item6->Add(item8, 1, wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL|wxALL, 0);
 
@@ -131,13 +145,15 @@ void wxChatDialog::CreateControls()
     item3->Add(item9, 1, wxALIGN_RIGHT|wxGROW|wxALL, 0);
 
     wxListCtrl* item10 = new wxListCtrl;
-    item10->Create( item1, ID_LISTCTRL, wxDefaultPosition, wxSize(150, 100), wxLC_LIST  );
+    item10->Create( item1, UsuariosListCtrlID, wxDefaultPosition, wxSize(200, 100), wxLC_REPORT|wxLC_SINGLE_SEL|wxLC_HRULES|wxLC_VRULES );
+    UsuariosListCtrl = item10;
     item9->Add(item10, 1, wxALIGN_RIGHT|wxALL, 0);
 
     item9->Add(5, 5, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 0);
 
     wxListCtrl* item12 = new wxListCtrl;
-    item12->Create( item1, ID_LISTCTRL1, wxDefaultPosition, wxSize(150, 100), 0 );
+    item12->Create( item1, CanalesListCtrlID, wxDefaultPosition, wxSize(200, 100), 0 );
+    CanalesListCtrl = item12;
     item9->Add(item12, 0, wxALIGN_RIGHT|wxALL, 0);
 
 ////@end wxChatDialog content construction
@@ -151,3 +167,32 @@ bool wxChatDialog::ShowToolTips()
 {
     return TRUE;
 }
+/*!
+ * wxEVT_COMMAND_BUTTON_CLICKED event handler for EnviarButton
+ */
+
+void wxChatDialog::OnEnviarbuttonClick( wxCommandEvent& event )
+{
+    // Insert custom code here
+    UsuariosListCtrl->InsertColumn(0,_("Nombre"));
+    UsuariosListCtrl->InsertColumn(1,_("Valor"));
+    UsuariosListCtrl->InsertColumn(2,_("Ranking"));
+    UsuariosListCtrl->InsertItem(0,_T("0,0"));
+    UsuariosListCtrl->SetItem(0,1,_T("0,1"));
+    UsuariosListCtrl->SetItem(0,2,_T("0,2"));
+    UsuariosListCtrl->InsertItem(1,_T("1,0"));
+    UsuariosListCtrl->SetItem(1,1,_T("1,1"));
+    UsuariosListCtrl->SetItem(1,2,_T("1,2"));
+    UsuariosListCtrl->InsertItem(2,_T("2,0"));
+    UsuariosListCtrl->SetItem(2,1,_T("2,1"));
+    UsuariosListCtrl->SetItem(2,2,_T("2,2"));
+    UsuariosListCtrl->SetColumnWidth(0,wxLIST_AUTOSIZE);
+    UsuariosListCtrl->SetColumnWidth(1,wxLIST_AUTOSIZE);
+    UsuariosListCtrl->SetColumnWidth(2,wxLIST_AUTOSIZE);
+    UsuariosListCtrl->InsertItem(3,_T("Nombre largo"));
+    UsuariosListCtrl->SetItem(3,1,_T("3,1"));
+    UsuariosListCtrl->SetItem(3,2,_T("3,2"));
+    event.Skip();
+}
+
+
