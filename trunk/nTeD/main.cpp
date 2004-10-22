84499bcd6111073c86649a17edca830051ea20d0
kintups <kintups@62037c61-b3d6-0310-b8c7-ccd26a683bc2>
2004-10-22 09:49:57 +0000

    Added initialization code for sockets (solves 100% CPU usage). Modified minimum size for game window.

--- trunk/nTeD/main.cpp
+++ trunk/nTeD/main.cpp
@@ -12,11 +12,12 @@ MainApp::OnInit()
 {
 		wxInitAllImageHandlers();
 		wxFileSystem::AddHandler(new wxInternetFSHandler);
+		wxSocketBase::Initialize();
 //		wxXmlResource::Get()->InitAllHandlers();
 //    wxXmlResource::Get()->Load("nted.xrc");
     frame = new wxMainFrame(NULL);
     frame->Show(TRUE);
-    frame->SetSizeHints(684,380,1600,1200);
+    frame->SetSizeHints(684,433,1600,1200);
     frame->Layout();
     frame->Fit();
     frame->Center();
